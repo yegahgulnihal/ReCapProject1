cc382acc6709c4bfdd825e00375c26f419c23381
yegahgulnihal <yegahdegirmenci@gmail.com>
2021-02-20 11:01:49 +0300

    20/02/2021

---
+++ Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    class CarValidator :AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(c => c.ModelName).NotEmpty();
+            RuleFor(c => c.ModelYear).NotEmpty();
+            RuleFor(c => c.ModelYear).GreaterThan(1900);
+            RuleFor(c => c.DailyPrice).NotEmpty();
+            RuleFor(c => c.DailyPrice).GreaterThan(100);
+        }
+    }
+}
